10ed4eb0908c498641334118855b22ce55a4aeb4
Jakub Migda <127434883+Jakub0821@users.noreply.github.com>
2024-08-31 01:08:12 +0200

    change

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const calendarElement = document.getElementById('calendar');
+    calendarElement.innerHTML = "<p>Here will be your calendar</p>";
+
+    // Tutaj możesz dodać logikę JavaScript do obsługi kalendarza wizyt
+});
