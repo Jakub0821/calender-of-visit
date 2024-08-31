000badf523a78ac571510cc95a2fa36210a8fdb0
Jakub Migda <127434883+Jakub0821@users.noreply.github.com>
2024-08-31 20:35:52 +0200

    change and add

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<db_jakubmigda120@gmail.com>:<db_m^u+pR-6$8gbqQS>@cluster2.pl88e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.log('MongoDB connection error:', err));
+
 document.addEventListener("DOMContentLoaded", function() {
     const calendarElement = document.getElementById('calendar');
     calendarElement.innerHTML = "<p>Here will be your calendar</p>";
@@ -110,3 +119,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     
 });
+
