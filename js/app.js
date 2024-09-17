494bba4e10c96677e76f48efac94bde3d272b152
Jakub Migda <127434883+Jakub0821@users.noreply.github.com>
2024-09-17 22:17:55 +0200

    change app.js

--- js/app.js
+++ js/app.js
@@ -8,12 +8,13 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://<db_jakubmigda120@gmail.com>:<db_m^u+pR-6$8gbqQS>@cluster2.pl88e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect('mongodb+srv://jakubmigda120:master2@cluster2.pl88e.mongodb.net/myDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.log('MongoDB connection error:', err));
+
 
 // Appointment Schema and Model
 const appointmentSchema = new mongoose.Schema({
