e0bdb59b29cca573dcbb5b5b00387ffe966eabb3
Jakub Migda <127434883+Jakub0821@users.noreply.github.com>
2024-09-18 20:36:43 +0200

    change

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://calendarUser:3MfZUlqGzxoqK7sY@cluster2.pl88e.mongodb.net/appointmentsCalendar?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  
+mongoose.connect('mongodb+srv://calendarUser:3MfZUlqGzxoqK7sY@cluster2.pl88e.mongodb.net/appointmentsCalendar', {
+  retryWrites: true,
+  w: 'majority'
+})
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.log('MongoDB connection error:', err));
+
 
 // Use the appointment routes
 app.use('/api', appointmentRoutes);
